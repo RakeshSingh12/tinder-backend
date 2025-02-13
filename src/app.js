fbbd637bd7aee319cdecbfb2ac65b3c5cd819c91
Rakesh Singh <36115281+RakeshSingh12@users.noreply.github.com>
2025-02-13 15:53:45 +0530

    port update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
 const app = express();
+const port = 7777;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use('/test', (req, res) => {
+    res.send(`<h1 style="color: green;">Hello World!</h1>`);
 })
 
-app.listen(7777, () =>{
+app.use((req, res) => {
+    res.send(`<h1 style="color: green;">Hello World!</h1>`);
+})
+
+app.listen(port, () =>{
     console.log("listening the server")
 })
