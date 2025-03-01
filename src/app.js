32e6486450a2c37ead8e94b97c2694fb684d26ca
Rakesh Singh <36115281+RakeshSingh12@users.noreply.github.com>
2025-03-02 00:46:40 +0530

    get, delete and put api added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 
 //import mongoDB database
-require('./config/database');
+require("./config/database");
 const connectDB = require("./config/database");
 const User = require("./module/user");
 // import middleware file
@@ -10,29 +10,92 @@ const app = express();
 app.use(express.json());
 const port = 7777;
 
-
 app.post("/signup", async (req, res) => {
-    
-    //Create a new instance of the user Model
-    const user = new User(req.body);
+  //Create a new instance of the user Model
+  const user = new User(req.body);
 
-    try {
-        await user.save();
-        res.send("User saved successfully")
-    } catch (err) { 
-        res.status(400).send("Failed to save user"+ err.message);
-    }
-})
-
-
-connectDB().then(() =>{
-    console.log("MongoDB database connection established...");
-})
-.catch((err) =>{
-    console.error("Database connection error", err);
+  try {
+    await user.save();
+    res.send("User saved successfully");
+  } catch (err) {
+    res.status(400).send("Failed to save user" + err.message);
+  }
 });
 
+// Retrieve a specific user by email from the database
+app.get("/user", async (req, res) => {
+  const email = req.body.email;
+  try {
+    const userEmail = await User.find({ email: email });
+    if (userEmail.length === 0) {
+      res.status(404).send("User not found"); // return a 404 Not Found status code with a message
+    } else {
+      res.send(userEmail);
+    }
+  } catch (err) {
+    res.status(400).send("Failed to get user" + err.message);
+  }
+});
 
-app.listen(port, () =>{
-    console.log("listening the server")
-})
+// delete specific user details from the database by userId
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    //const user =  await User.findByIdAndDelete({_id : userId});
+    if (!user) {
+      res.status(404).send("User not found"); // return a 404 Not Found status code with a message
+    } else {
+      res.send("User deleted successfully");
+    }
+  } catch (err) {
+    res.status(400).send("Failed to delete user" + err.message);
+  }
+});
+
+// update specific user details from the database by userId
+
+app.put("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const updatedUser = req.body;
+  console.log(updatedUser)
+
+  try {
+    const user = await User.findByIdAndUpdate((userId), updatedUser);
+    if (!user) {
+      res.status(404).send("User data not update"); // return a 404 Not Found status code with a message
+    } else {
+      res.send("user data updated successfully");
+    }
+  } catch (err) {
+    res.status(400).send("Failed to update user -->" + err.message);
+  }
+});
+
+// Retrieve a all uset data from the  database
+app.get("/feeds", async (req, res) => {
+  try {
+    const user = await User.find();
+    if (!user) {
+      res.status(404).send("No user found"); // return a 404 Not Found status code with a message
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(404).send(" something went wrong.");
+  }
+});
+
+connectDB()
+  .then(() => {
+    console.log("MongoDB database connection established...");
+  })
+  .catch((err) => {
+    console.error("Database connection error", err);
+  });
+
+app.listen(port, () => {
+  console.log("listening the server");
+});
