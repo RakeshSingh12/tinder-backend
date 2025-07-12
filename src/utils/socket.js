17363309307b62f2e4253667059e257001dfd075
Rakesh Singh <36115281+RakeshSingh12@users.noreply.github.com>
2025-07-12 23:23:03 +0530

    Socket IO Event handlers

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,21 +1,28 @@
-
-const socket = require('socket.io');
+const socket = require("socket.io");
 // This module initializes a WebSocket server using Socket.IO.
 const initializeSocket = (server) => {
-    const io = socket( server, {
-        cors: {
-            origin: "http://localhost:3000",
-        },
-    })
-    io.on("connection", (socket) => {
-        console.log("New client connected");
+  const io = socket(server, {
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
+    // This function is called when a new client connects to the WebSocket server.
+  io.on("connection", (socket) => {
+    console.log("New client connected");
 
-        socket.on("disconnect", () => {
-            console.log("Client disconnected");
-        });
-
-        // You can add more event listeners here
+    // Handle incoming messages from the client(Event handlers)
+    socket.on("joinChat", () => {
+      console.log("Client joined chat");
     });
-}
+    socket.on("sendMessage", () => {
+      console.log("Message received from client");
+    });
+    socket.on("disconnect", () => {
+      console.log("Client disconnected");
+    });
+
+    // You can add more event listeners here
+  });
+};
 
 module.exports = initializeSocket;
