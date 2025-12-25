a8c5253b64c93fd586fdc97125d963de74bfd7ac
Rakesh Singh <36115281+RakeshSingh12@users.noreply.github.com>
2025-12-25 23:36:28 +0530

    style: fix lint errors in health.test.js

--- src/tests/health.test.js
+++ src/tests/health.test.js
@@ -1,9 +1,11 @@
 const request = require("supertest");
 
 // Mock external services to avoid initialization errors when env vars are missing
-jest.mock('../utils/razorpay', () => ({}));
-jest.mock('../utils/sesClient', () => ({ sesClient: { send: jest.fn() } }));
-jest.mock('../utils/cornJob', () => ({ schedulePendingRequestsJob: jest.fn() }));
+jest.mock("../utils/razorpay", () => ({}));
+jest.mock("../utils/sesClient", () => ({ sesClient: { send: jest.fn() } }));
+jest.mock("../utils/cornJob", () => ({
+  schedulePendingRequestsJob: jest.fn(),
+}));
 
 const app = require("../app");
 
